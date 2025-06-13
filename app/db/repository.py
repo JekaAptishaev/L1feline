8c7fefef1180e4e6cfc697f2214c554d3e5acb66
Djelal <jelyalaga@gmail.com>
2025-06-13 12:32:30 +0300

    всегда спрашивается ФИО при входе

--- app/db/repository.py
+++ app/db/repository.py
@@ -14,7 +14,7 @@ class UserRepo:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_or_create_user(self, telegram_id: int, username: str, first_name: str, last_name: str | None) -> User:
+    async def get_or_create_user(self, telegram_id: int, username: str | None, first_name: str | None, last_name: str | None) -> User:
         try:
             logger.info(f"Попытка получить пользователя с telegram_id={telegram_id}")
             stmt = (
